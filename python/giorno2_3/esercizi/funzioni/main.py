df52fa5264b2d44951ead91eea1a695e628137b9
Erica Cutuli <ericacutuli@gmail.com>
2025-01-27 16:16:07 +0100

    Esercizi svolti

--- python/giorno2_3/esercizi/funzioni/main.py
+++ python/giorno2_3/esercizi/funzioni/main.py
@@ -1,4 +1,38 @@
 # Scrivere il codice dell'esercizi qui dentro
 
 def pow_list(seq):
-    ...
+    return [x**2 for x in seq]
+
+def count_words(s):
+    conteggio = s.split(' ')
+    return len(conteggio)
+
+def reverse_string(s):
+    return s[::-1]
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+def sum_even_numbers(seq):
+    return sum(x for x in seq if x % 2 == 0)
+
+def find_max(seq):
+    max_value = seq[0]
+    for x in seq:
+        if x > max_value:
+            max_value = x
+    return max_value
+
+def count_vowels(s):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for char in s.lower():
+        if char in vowels:
+            count += 1
+    return count
